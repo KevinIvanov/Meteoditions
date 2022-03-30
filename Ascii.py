d2e84a2f6898618c4c6efc730f787647ea708cdc
Kevin Ivanov <87090118+KevinIvanov@users.noreply.github.com>
2022-03-30 15:04:40 -0300

    Create Ascii.py

---
+++ Ascii.py
@@ -0,0 +1,18 @@
+from colorama import Fore
+
+# Logo
+
+print(rf"""{Fore.MAGENTA}
+
+___  ___     _                 _ _ _   _                 
+|  \/  |    | |               | (_) | (_)                
+| .  . | ___| |_ ___  ___   __| |_| |_ _  ___  _ __  ___ 
+| |\/| |/ _ \ __/ _ \/ _ \ / _` | | __| |/ _ \| '_ \/ __|
+| |  | |  __/ ||  __/ (_) | (_| | | |_| | (_) | | | \__ \
+\_|  |_/\___|\__\___|\___/ \__,_|_|\__|_|\___/|_| |_|___/
+                                
+                                  Created by Kevin Ivanov
+
+ ------------ Using wttr.in a console-oriented weather forecast ------------
+
+{Fore.RESET}""")
